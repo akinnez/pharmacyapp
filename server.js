daf630e8a7d87323c80031aefb17dbf2fe242099
akinnez <oyetunjiidris@gmail.com>
2022-08-10 16:02:40 +0200

    new server

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
- function redirectHTTPS(req, res,next){
-    if(!req.secure && req.get('x-forwarded-photo') !== 'https'){
-        return 
-    }
-    res.redirect('https://' + req.get('host') + req.url)
-    next()
- }
+//  function redirectHTTPS(req, res,next){
+//     if(!req.secure && req.get('x-forwarded-photo') !== 'https'){
+//         return 
+//     }
+//     res.redirect('https://' + req.get('host') + req.url)
+//     next()
+//  }
 
  const express = require('express')
  const  app = express()
  
- app.use(redirectHTTPS)
+//  app.use(redirectHTTPS)
 
  app.use(express.static('./dist/pharmacyapp'))
  app.get('/*',(req,res)=> res.sendFile('index.html',{root: './dist/pharmacyapp'}))
